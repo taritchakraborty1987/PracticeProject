de84ad3cdd5786bcb77753ac4e9fc690bb12ea2c
tarit <tarit@DESKTOP-01MSPKF>
2022-08-25 08:37:48 +0530

    New

---
+++ PracticeProject/src/sampleJavaCode/ProgSwitchCase.java
@@ -0,0 +1,61 @@
+package sampleJavaCode;
+
+public class ProgSwitchCase {
+
+	public static void main(String[] args) {
+		int i=8;
+		switch (i) {
+			case 0:
+				System.out.println("Zero");
+				break   ;
+			case 1:
+				System.out.println("One");
+				break;
+			case 2:
+				System.out.println("Two");
+				break;
+			case 3:
+				System.out.println("Three");
+				break;
+			case 4:
+				System.out.println("Four");
+				break;
+			case 5:
+				System.out.println("Five");
+				break;
+			case 6:
+				System.out.println("Six");
+				break;
+			case 7:
+				System.out.println("Seven");
+				break;
+			case 8:
+				System.out.println("Eight");
+				break;
+			case 9:
+				System.out.println("Nine");
+				break;
+			case 10:
+				System.out.println("Ten");
+				break;
+			default:
+				System.out.println("Not Matching");
+				break;
+		}
+		
+		String varS="Hello";
+		switch (varS) {
+			case "Hi":
+				System.out.println("Hi");
+				break   ;
+			case "Hello":
+				System.out.println("Hello");
+				break   ;
+			default:
+				System.out.println("Not Matching");
+				break;
+		}
+
+	}
+
+}
